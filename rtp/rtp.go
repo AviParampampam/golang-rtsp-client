d494baaa4b083fab79896433972fb35a15010218
AviParampampam <aviparampampam@gmail.com>
2021-01-13 16:58:33 +0500

    ..

--- rtp/rtp.go
+++ rtp/rtp.go
@@ -32,6 +32,8 @@ func Listen(port int) {
 	defer ServerConn.Close()
 
 	buf := make([]byte, 4096)
+
+	counter := 0
 	for {
 		n, _, err := ServerConn.ReadFromUDP(buf)
 		if err != nil {
@@ -42,26 +44,33 @@ func Listen(port int) {
 		cpy := make([]byte, n)
 		copy(cpy, buf)
 		go handleBuf(cpy)
+
+		counter++
+		if counter > 1 {
+			break
+		}
 	}
 }
 
 func handleBuf(buf []byte) {
-	Version := buf[0] & 0x03
-	// Padding := buf[0]&1<<2 != 0
-	// Ext := buf[0]&1<<3 != 0
-	// CSRC := make([]uint, buf[0]>>4)
-	// Marker := buf[1]&1 != 0
-	// PayloadType := buf[1] >> 1
-	// SequenceNumber := toUint(buf[2:4])
-	// Timestamp := toUint(buf[4:8])
-	// SyncSource := toUint(buf[8:12])
-	// fmt.Println(Version, Padding, Ext, CSRC, Marker, PayloadType, SequenceNumber, Timestamp, SyncSource)
-	fmt.Println(Version)
-}
+	/*
+		Version := buf[0] & 0x03
+		Padding := buf[0]&1<<2 != 0
+		Ext := buf[0]&1<<3 != 0
+		CSRC := make([]uint, buf[0]>>4)
+		Marker := buf[1]&1 != 0
+		PayloadType := buf[1] >> 1
+		SequenceNumber := toUint(buf[2:4])
+		Timestamp := toUint(buf[4:8])
+		SyncSource := toUint(buf[8:12])
+		fmt.Println(Version, Padding, Ext, CSRC, Marker, PayloadType, SequenceNumber, Timestamp, SyncSource)
+	*/
+	V := buf[0] >> 4
+	P := buf[2]
+	X := buf[3]
+	CSRS := buf[4:8]
 
-func toUint(arr []byte) (ret uint) {
-	for i, b := range arr {
-		ret |= uint(b) << (8 * uint(len(arr)-i-1))
-	}
-	return ret
+	fmt.Println(V, P, X, CSRS)
+
+	fmt.Println(buf)
 }
